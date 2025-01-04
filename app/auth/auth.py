5edbec2762cb9e56f1b6703223ff345ccb078560
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2025-01-04 14:24:20 +0300

    Made the structure based on full example from docs

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,3 +1,5 @@
+from fastapi_users.authentication import JWTStrategy, BearerTransport, AuthenticationBackend
+from config import config
 import uuid
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, UUIDIDMixin
@@ -5,7 +7,19 @@ from fastapi_users import BaseUserManager, UUIDIDMixin
 from database import User, get_user_db
 from config import config
 
+bearer_transport = BearerTransport(tokenUrl="oauth/jwt/login")
+
 SECRET = config.JWT_SECRET_KEY
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+oauth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
+
 
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     reset_password_token_secret = SECRET
