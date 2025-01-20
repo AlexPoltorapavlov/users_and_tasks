855b41e8fd30fb6f05258f6c01e142e54167021a
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2025-01-20 18:05:55 +0300

    test(auth): make test_parse_id

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,11 @@
 import pytest
 from unittest.mock import patch
 
+@pytest.fixture(scope="function")
+def mock_user():
+    from app.auth.auth import User
+    return User(id=1, email="test@test.com", hashed_password="hashed_password", is_active=True, is_superuser=False, is_verified=False)
+
 def test_get_jwt_strategy():
     from app.auth.auth import get_jwt_strategy
     SECRET = "SECRET"
@@ -11,3 +16,17 @@ def test_get_jwt_strategy():
         assert jwt_strategy.lifetime_seconds == 3600
         assert jwt_strategy.token_audience == ["fastapi-users:auth"]
         assert jwt_strategy.algorithm == "HS256"
+
+def test_parse_id(mock_user):
+    from app.auth.auth import UserManager
+    user_manager = UserManager(mock_user)
+
+    assert user_manager.parse_id(2) == 2
+    assert user_manager.parse_id("2") == 2
+
+    with pytest.raises(ValueError):
+        user_manager.parse_id("a")
+    with pytest.raises(ValueError):
+        user_manager.parse_id("2.0")
+    with pytest.raises(TypeError):
+        user_manager.parse_id()
