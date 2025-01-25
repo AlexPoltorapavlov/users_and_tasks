20303bb3ba61a286d12ac0c6431c22a084d57a8c
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2025-01-25 22:29:08 +0300

    docs(app.dal.user): title

--- app/dal/user.py
+++ app/dal/user.py
@@ -3,7 +3,25 @@ from app.models.models import User
 from sqlalchemy import select
 
 class UserRepository(SQLAlchemyUserDatabase):
+    """Repository class for handling user-related database operations.
+
+    This class extends SQLAlchemyUserDatabase to provide additional functionality
+    for user management in the database.
+
+    Attributes:
+        session: SQLAlchemy async session object for database operations.
+    """
+
     async def get_all(self):
+        """Retrieves all users from the database.
+
+        Returns:
+            list[User]: A list of all User objects in the database.
+
+        Example:
+            >>> repo = UserRepository(session)
+            >>> users = await repo.get_all()
+        """
         async with self.session as session:
             result = await session.execute(select(User))
             users = result.scalars().all()
