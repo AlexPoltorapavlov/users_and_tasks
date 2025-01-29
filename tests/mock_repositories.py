70507855be52b7fcfa1ed5ef857eb08f66d3c5f7
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2025-01-29 11:28:39 +0300

    test(test_managers): add test_create_task

--- tests/mock_repositories.py
+++ tests/mock_repositories.py
@@ -1,12 +1,57 @@
 from pytest_asyncio import fixture as async_fixture
+import pytest
 from app.repositories import TaskRepository
 from unittest.mock import AsyncMock
 from app.models import Task, User
 from app.managers.task import TaskManager
 
+#########
+# Фикстуры с задачами
+#########
+
+@pytest.fixture
+def get_base_task():
+    mock_task = Task(
+        id=1,
+        name="Test Task",
+        description="Test Description",
+        status="new",
+        user_id=1
+    )
+    return mock_task
+
+@pytest.fixture
+def get_update_task():
+    mock_task = Task(
+        id=1,
+        name="Updated Test Task",
+        description="Updated Test Description",
+        status="in_progress",
+        user_id=1
+    )
+    return mock_task
+
+##########
+# Фикстуры для получения объекта TaskManager
+##########
+
 @async_fixture
-async def mock_get_task_repository():
-    return AsyncMock(spec=TaskRepository)
+async def task_repository(get_base_task):
+    """
+    Args: 
+        get_base_task(pytest.fixture) with base task example
+        
+    Returning: task_repository (AsyncMock) - mock of TaskRepository object
+    """
+
+    task_repository = AsyncMock(spec=TaskRepository)
+    base_task = get_base_task
+    task_repository.create_task = AsyncMock(return_value=base_task)
+    return task_repository
+
+@async_fixture
+async def mock_get_task_repository(task_repository):
+    return task_repository
 
 @async_fixture
 async def mock_get_task_manager(mock_get_task_repository):
--- tests/test_managers.py
+++ tests/test_managers.py
@@ -1,10 +1,8 @@
 import pytest_asyncio
 import pytest
 from app.managers.task import TaskManager, get_task_manager
-from .mock_repositories import (
-    mock_get_task_manager,
-    mock_get_task_repository
-)
+from .mock_repositories import *
+from app.schemas.tasks import *
 
 
 @pytest.mark.asyncio
@@ -12,3 +10,23 @@ async def test_get_task_manager(mock_get_task_repository):
     task_manager_generator = get_task_manager(mock_get_task_repository)
     task_manager = await task_manager_generator.__anext__()
     assert isinstance(task_manager, TaskManager)
+
+@pytest.mark.asyncio
+async def test_create_task(mock_get_task_manager):
+    task_manager = mock_get_task_manager
+    task_data = TaskCreate(
+        name="Test Task",
+        description="Test Description",
+        status="new",
+        user_id=1  # Убедитесь, что user_id передается, если требуется
+    )
+    
+    result = await task_manager.create_task(task_data)
+    
+    # Проверяем, что метод create_task репозитория был вызван
+    task_manager.task_db.create_task.assert_awaited_once_with(task_data)
+    
+    # Проверяем, что результат соответствует ожиданиям
+    assert isinstance(result, TaskRead)
+    assert result.id == 1
+    assert result.name == "Test Task"
